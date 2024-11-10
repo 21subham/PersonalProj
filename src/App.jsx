61c8991e9853d4c6f300429d5c2c058f6fe4de63
21subham <subhamrajbhandari@gmail.com>
2024-11-10 16:11:54 +0545

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
 
 function App() {
   const [box, setBox] = useState(false);
@@ -16,7 +17,9 @@ function App() {
   };
 
   return (
-    <>
+    <HashRouter>
+      {" "}
+      {/* Wrap the app with HashRouter */}
       <div className="h-screen flex justify-center items-center">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700 text-center">
           <h1 className="text-white text-center my-3 text-2xl">
@@ -25,10 +28,9 @@ function App() {
 
           {/* for buttons */}
           <div className="flex justify-center space-x-4">
-            {" "}
             {box ? (
               <>
-                {/* "No" button*/}
+                {/* "No" button */}
                 <button
                   className="outline-none bg-blue-500 py-0.5 px-8 mx-3 shrink-0 text-white rounded-xl active:bg-blue-700 active:scale-95 active:shadow-inner"
                   onClick={() => changer(true)}
@@ -36,7 +38,7 @@ function App() {
                   No
                 </button>
 
-                {/* "Yes" button*/}
+                {/* "Yes" button */}
                 <button
                   onClick={handleYesClick}
                   className="outline-none bg-blue-500 py-0.5 px-8 mx-3 shrink-0 text-white rounded-xl active:bg-blue-700 active:scale-95 active:shadow-inner"
@@ -46,7 +48,7 @@ function App() {
               </>
             ) : (
               <>
-                {/* "Yes" button*/}
+                {/* "Yes" button */}
                 <button
                   onClick={handleYesClick}
                   className="outline-none bg-blue-500 py-0.5 px-8 mx-3 shrink-0 text-white rounded-xl active:bg-blue-700 active:scale-95 active:shadow-inner"
@@ -66,7 +68,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </HashRouter>
   );
 }
 
